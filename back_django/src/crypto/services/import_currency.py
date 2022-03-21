d0bfa959ba4c37830e4b45d77ea9664220f62e63
Romain PIOT <romainpiot.pro@gmail.com>
2022-03-21 23:46:20 +0100

    loop saving historical

--- back_django/src/crypto/services/import_currency.py
+++ back_django/src/crypto/services/import_currency.py
@@ -16,6 +16,18 @@ def fetch_all_candles(exchange, max_retries, symbol, timeframe, since, limit):
     retry_nb = 0
     candles = []
 
+    exchangeObj = Exchange.objects.get(name=exchange)
+    symbolPair = symbol.split("/")
+    currencyObj = Currency.objects.get(slug=symbolPair[0])
+    toCurrencyObj = Currency.objects.get(slug=symbolPair[1])
+
+    # TODO if last inserted > date requested => start from last
+    symbolObj, created = Symbol.objects.get_or_create(
+        from_exchange=exchangeObj,
+        currency=currencyObj,
+        to_currency=toCurrencyObj,
+    )
+
     while True:
         ohlcv = retry_fetch_ohlcv(exchange, symbol, timeframe, since, limit)
 
@@ -26,34 +38,24 @@ def fetch_all_candles(exchange, max_retries, symbol, timeframe, since, limit):
         retry_nb += 1
 
         if retry_nb >= max_retries:
-            save_candles(exchange, symbol, timeframe, candles)
+            since = save_candles(exchangeObj, symbolObj, timeframe, candles)
+            print(since)
+            candles = []
             retry_nb = 0
 
     return True
 
 
-def save_candles(exchange, symbol, timeframe, candles):
+def save_candles(exchangeObj, symbolObj, timeframe, candles):
+    last_datetime = candles[-1][0]
+
     candles = pd.DataFrame(
         data=candles,
         columns=["datetime", "open", "high", "low", "close", "volume"],
     )
     candles["datetime"] = candles["datetime"].apply(lambda x: dt.fromtimestamp(int(x) / 1000))
-    # candles = candles.sort_values(by=["datetime"], ascending=[True])
-    # candles = candles.set_index("datetime")
 
-    exchangeObj = Exchange.objects.get(name=exchange)
-    symbol = symbol.split("/")
-
-    currencyObj = Currency.objects.get(slug=symbol[0])
-    toCurrencyObj = Currency.objects.get(slug=symbol[1])
-
-    # TODO move some object in fetch_all_candles
-    # TODO if last inserted > date requested => start from last
-    symbolObj, created = Symbol.objects.get_or_create(
-        from_exchange=exchangeObj,
-        currency=currencyObj,
-        to_currency=toCurrencyObj,
-    )
+    historical_list = []
 
     for _, candle in candles.iterrows():
         historical = Historical(
@@ -68,15 +70,19 @@ def save_candles(exchange, symbol, timeframe, candles):
             volume=candle["volume"],
         )
 
-        # save historical
-        historical.save()
+        historical_list.append(historical)
+
+    # update all historical
+    Historical.objects.bulk_create(historical_list)
 
     # update Symbol last insertion
     symbolObj.last_imported = candles.iloc[-1]["datetime"]
     symbolObj.save()
 
+    return last_datetime
 
-def import_candles(symbol, since: dt = dt(2012, 1, 1, 0, 0, 0, 0), limit=10, exchange_id="binance", max_retries=3, timeframe="1m"):
+
+def import_candles(symbol, since: dt = dt(2012, 1, 1, 0, 0, 0, 0), limit=1000, exchange_id="binance", max_retries=3, timeframe="1m"):
 
     symbol = symbol.upper()
     since_datetime = int(since.timestamp() * 1000)
@@ -85,6 +91,3 @@ def import_candles(symbol, since: dt = dt(2012, 1, 1, 0, 0, 0, 0), limit=10, exc
     exchange = exchange_class({"apiKey": os.environ.get("BINANCE_API_KEY"), "secret": os.environ.get("BINANCE_API_SECRET")})
 
     fetch_all_candles(exchange, max_retries, symbol, timeframe, since_datetime, limit)
-
-    # for candle in candles:
-    #     print(candle)
